4a7fcae9ee88843ef7cbcd843e1b8b21505fb0db
mahi6535@gmail.com <mahi6535@gmail.com>
2019-09-14 18:29:49 +0530

    added routes

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  twilioConfig: {
+    //Twilio API configuration (trail credentials)
+    accountSid: 'ACdc02315ad8c5c2983481c5f87517463c',
+    authToken: 'e8ca62b206d3480cbabb57fdf272e041',
+    appSid: 'AP97d8e0e31d680087eea089ab91475fab',
+    number: '+15042245375'
+  },
+  callback_domain: 'http://57ee9131.ngrok.io',
+  apiKey: 'acta.WRtGK4OJsqq0aCQ_0dFm7nH18XI'
+};
--- server.js
+++ server.js
@@ -11,17 +11,41 @@ var cookieParser = require('cookie-parser');
 var flash = require('connect-flash');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+
+var home = require('./routes/home');
+var call = require('./routes/call');
+var dashboard = require('./routes/dashboard');
+// var tickets = require('./routes/tickets');
+var token = require('./routes/token');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-app.get('/', function(req, res) {
-  res.send({
-    Output: 'Hello World again!'
-  });
+var port = process.env.PORT || 3000;
+// app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'secret',
+    name: 'browser-calls',
+    cookie: { maxAge: 60000 },
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+// middleware for flash message handling
+app.use(function(req, res, next) {
+  res.locals.success = req.flash('success');
+  res.locals.errors = req.flash('errors');
+  next();
 });
+app.use('/', home);
 
 app.post('/', function(req, res) {
   res.send({
