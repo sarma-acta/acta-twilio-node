0a8229452336f6744df4da5bac85f4d13bfdaee5
mahi6535@gmail.com <mahi6535@gmail.com>
2019-09-14 18:17:17 +0530

    serverjs updated with new packages

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// global.db = require('./models');
+global.config = require('./config/config');
+var cookieParser = require('cookie-parser');
+var flash = require('connect-flash');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
   res.send({
